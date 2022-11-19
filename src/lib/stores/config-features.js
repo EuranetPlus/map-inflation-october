c47d517dea0b16e12400263219de2634be56fb17
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2022-11-19 19:20:46 +0200

    Update config-features.js

--- src/lib/stores/config-features.js
+++ src/lib/stores/config-features.js
@@ -2,7 +2,7 @@ export const config = {
   "mapTitle": "map-inflation-october", // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
   "vercelURL": "map-inflation-october.vercel.app", // The url provided when deploying the map on Vercel
   "datasetType": "values", // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
-  "datasetUnit": "fullNumbers", // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
+  "datasetUnit": "percent", // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
   "colourScheme": "orange-red", // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
   "headlineAvailable": true, // true or false; Should the map have a headline? 
   "subheadlineAvailable": true, // true or false; Should the map have a subheadline? 
